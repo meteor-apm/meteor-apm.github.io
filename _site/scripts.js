4971936e303431b9af96a054044f75adb054e23f
Muhammed Thanish <mnmtanish@gmail.com>
2014-05-22 09:23:07 +0530

    Remove generated site

--- _site/scripts.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-  var main = document.getElementById('main');
-  var header = document.getElementById('topbar');
-  var footer = document.getElementById('footer');
-  var height = window.innerHeight - header.getBoundingClientRect().height - footer.getBoundingClientRect().height;
-  main.style.minHeight = height + 'px';
-});
