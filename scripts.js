9df301a5697dae2d9c66d0e6311b8c023f03b9ac
Muhammed Thanish <mnmtanish@gmail.com>
2014-05-22 10:21:14 +0530

    Rename topbar to header

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
   var main = document.getElementById('main');
   if(!main) return;
-  var header = document.getElementById('topbar');
+  var header = document.getElementById('header');
   var footer = document.getElementById('footer');
   var height = window.innerHeight - header.getBoundingClientRect().height - footer.getBoundingClientRect().height;
   main.style.minHeight = height + 'px';
